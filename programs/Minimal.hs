a1a7a4d5f0e8f7a9d68b989f80baca60acd78752
Jonathan Daugherty <cygnus@foobox.com>
2015-06-28 12:36:16 -0700

    Simplify minimal demo

--- programs/Minimal.hs
+++ programs/Minimal.hs
@@ -1,29 +1,11 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
-import Data.Monoid ((<>))
-import Graphics.Vty
-import Data.Text.Markup ((@@))
-
 import Brick.Main
-import Brick.Util
 import Brick.Widgets.Core
-import Brick.Markup
-import Brick.AttrMap
 
 ui :: Widget
-ui = m1 <=> m2
-    where
-        -- Two ways to assign attributes to text in markup: via
-        -- attributes (direct) or via attribute names (indirect)
-        m1 = markup $ ("Hello" @? "kw1") <> ", " <> ("world!" @? "kw2")
-        m2 = markup $ ("Hello" @@ fg red) <> ", " <> ("world!" @@ (yellow `on` black))
-
-aMap :: [(AttrName, Attr)]
-aMap =
-    [ ("kw1", fg green)
-    , ("kw2", red `on` black)
-    ]
+ui = "Hello, world!"
 
 main :: IO ()
-main = simpleMain aMap [ui]
+main = simpleMain [] [ui]
