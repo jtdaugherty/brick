37052daf97c9f7d8e443e6c8038d6f4255d22d0b
Jonathan Daugherty <cygnus@foobox.com>
2015-07-01 00:00:03 -0700

    Use s instead of a as App state type variable

--- src/Brick/Main.hs
+++ src/Brick/Main.hs
@@ -60,15 +60,15 @@ data Next a = Continue a
 -- | The library application abstraction. Your application's operations
 -- are represented here and passed to one of the various main functions
 -- in this module. An application is in terms of an application state
--- type 'a' and an application event type 'e'. In the simplest case 'e' is
+-- type 's' and an application event type 'e'. In the simplest case 'e' is
 -- vty's 'Event' type, but you may define your own event type, permitted
 -- that it has a constructor for wrapping Vty events, so that Vty events
 -- can be handled by your event loop.
-data App a e =
-    App { appDraw :: a -> [Widget]
+data App s e =
+    App { appDraw :: s -> [Widget]
         -- ^ This function turns your application state into a list of
         -- widget layers. The layers are listed topmost first.
-        , appChooseCursor :: a -> [CursorLocation] -> Maybe CursorLocation
+        , appChooseCursor :: s -> [CursorLocation] -> Maybe CursorLocation
         -- ^ This function chooses which of the zero or more cursor
         -- locations reported by the rendering process should be
         -- selected as the one to use to place the cursor. If this
@@ -76,15 +76,15 @@ data App a e =
         -- is that many widgets may request a cursor placement but your
         -- application state is what you probably want to use to decide
         -- which one wins.
-        , appHandleEvent :: e -> a -> EventM (Next a)
+        , appHandleEvent :: e -> s -> EventM (Next s)
         -- ^ This function takes an event and your application state
         -- and returns an action to be taken. Possible options are
         -- 'continue', 'suspendAndResume', and 'halt'.
-        , appStartEvent :: a -> EventM a
+        , appStartEvent :: s -> EventM s
         -- ^ This function gets called once just prior to the first
         -- drawing of your application. Here is where you can make
         -- initial scrolling requests, for example.
-        , appAttrMap :: a -> AttrMap
+        , appAttrMap :: s -> AttrMap
         -- ^ The attribute map that should be used during rendering.
         , appMakeVtyEvent :: Event -> e
         -- ^ The event constructor to use to wrap Vty events in your own
@@ -100,11 +100,11 @@ type EventState = [(Name, ScrollRequest)]
 -- | The default main entry point which takes an application and an
 -- initial state and returns the final state returned by a 'halt'
 -- operation.
-defaultMain :: App a Event
+defaultMain :: App s Event
             -- ^ The application.
-            -> a
+            -> s
             -- ^ The initial application state.
-            -> IO a
+            -> IO s
 defaultMain app st = do
     chan <- newChan
     customMain (mkVty def) chan app st
@@ -130,7 +130,7 @@ simpleMain w =
 -- a halt. This is a convenience function useful as an 'appHandleEvent'
 -- value for simple applications using the 'Event' type that do not need
 -- to get more sophisticated user input.
-resizeOrQuit :: Event -> a -> EventM (Next a)
+resizeOrQuit :: Event -> s -> EventM (Next s)
 resizeOrQuit e a =
     case e of
         EvResize _ _ -> continue a
@@ -139,7 +139,7 @@ resizeOrQuit e a =
 data InternalNext a = InternalSuspendAndResume RenderState (IO a)
                     | InternalHalt a
 
-runWithNewVty :: IO Vty -> Chan e -> App a e -> RenderState -> a -> IO (InternalNext a)
+runWithNewVty :: IO Vty -> Chan e -> App s e -> RenderState -> s -> IO (InternalNext s)
 runWithNewVty buildVty chan app initialRS initialSt = do
     withVty buildVty $ \vty -> do
         pid <- forkIO $ supplyVtyEvents vty (appMakeVtyEvent app) chan
@@ -165,11 +165,11 @@ customMain :: IO Vty
            -- ^ An event channel for sending custom events to the event
            -- loop (you write to this channel, the event loop reads from
            -- it).
-           -> App a e
+           -> App s e
            -- ^ The application.
-           -> a
+           -> s
            -- ^ The initial application state.
-           -> IO a
+           -> IO s
 customMain buildVty chan app initialAppState = do
     let run rs st = do
             result <- runWithNewVty buildVty chan app rs st
@@ -189,7 +189,7 @@ supplyVtyEvents vty mkEvent chan =
         e <- nextEvent vty
         writeChan chan $ mkEvent e
 
-runVty :: Vty -> Chan e -> App a e -> a -> RenderState -> IO (Next a, RenderState)
+runVty :: Vty -> Chan e -> App s e -> s -> RenderState -> IO (Next s, RenderState)
 runVty vty chan app appState rs = do
     firstRS <- renderApp vty app appState rs
     e <- readChan chan
@@ -201,7 +201,7 @@ withVty buildVty useVty = do
     vty <- buildVty
     useVty vty `finally` shutdown vty
 
-renderApp :: Vty -> App a e -> a -> RenderState -> IO RenderState
+renderApp :: Vty -> App s e -> s -> RenderState -> IO RenderState
 renderApp vty app appState rs = do
     sz <- displayBounds $ outputIface vty
     let (newRS, pic, theCursor) = renderFinal (appAttrMap app appState)
@@ -221,14 +221,14 @@ renderApp vty app appState rs = do
 -- process. This is a convenience function useful as an
 -- 'appChooseCursor' value when a simple application has no need to
 -- position the cursor.
-neverShowCursor :: a -> [CursorLocation] -> Maybe CursorLocation
+neverShowCursor :: s -> [CursorLocation] -> Maybe CursorLocation
 neverShowCursor = const $ const Nothing
 
 -- | Always show the first cursor, if any, returned by the rendering
 -- process. This is a convenience function useful as an
 -- 'appChooseCursor' value when a simple program has zero or more
 -- widgets that advertise a cursor position.
-showFirstCursor :: a -> [CursorLocation] -> Maybe CursorLocation
+showFirstCursor :: s -> [CursorLocation] -> Maybe CursorLocation
 showFirstCursor = const $ listToMaybe
 
 -- | A viewport scrolling handle for managing the scroll state of
@@ -253,17 +253,17 @@ viewportScroll n =
 
 -- | Continue running the event loop with the specified application
 -- state.
-continue :: a -> EventM (Next a)
+continue :: s -> EventM (Next s)
 continue = return . Continue
 
 -- | Halt the event loop and return the specified application state as
 -- the final state value.
-halt :: a -> EventM (Next a)
+halt :: s -> EventM (Next s)
 halt = return . Halt
 
 -- | Suspend the event loop, save the terminal state, and run the
 -- specified action. When it returns an application state value, restore
 -- the terminal state, and resume the event loop with the returned
 -- application state.
-suspendAndResume :: IO a -> EventM (Next a)
+suspendAndResume :: IO s -> EventM (Next s)
 suspendAndResume = return . SuspendAndResume
