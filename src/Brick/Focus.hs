a64f9db96f0f5e86afd69bd391f2621756c1601e
Jonathan Daugherty <cygnus@foobox.com>
2015-07-07 16:33:42 -0700

    Add docstrings to Brick.Focus

--- src/Brick/Focus.hs
+++ src/Brick/Focus.hs
@@ -1,3 +1,9 @@
+-- | This module provides a type and functions for handling focus rings
+-- of widgets. Note that this interface is merely provided for managing
+-- the focus state for a sequence of widget names; it does not do
+-- anything beyond keep track of that.
+--
+-- This interface is experimental.
 module Brick.Focus
   ( FocusRing
   , focusRing
@@ -13,30 +19,48 @@ import Data.Maybe (listToMaybe)
 
 import Brick.Core (Name(..), CursorLocation(..), cursorLocationName)
 
+-- | A focus ring containing a sequence of widget names to focus and a
+-- currently-focused widget name.
 data FocusRing = FocusRingEmpty
                | FocusRingNonempty ![Name] !Int
 
+-- | Construct a focus ring from the list of names.
 focusRing :: [Name] -> FocusRing
 focusRing [] = FocusRingEmpty
 focusRing names = FocusRingNonempty names 0
 
+-- | Advance focus to the next widget in the ring.
 focusNext :: FocusRing -> FocusRing
 focusNext FocusRingEmpty = FocusRingEmpty
 focusNext (FocusRingNonempty ns i) = FocusRingNonempty ns i'
     where
         i' = (i + 1) `mod` (length ns)
 
+-- | Advance focus to the previous widget in the ring.
 focusPrev :: FocusRing -> FocusRing
 focusPrev FocusRingEmpty = FocusRingEmpty
 focusPrev (FocusRingNonempty ns i) = FocusRingNonempty ns i'
     where
         i' = (i + (length ns) - 1) `mod` (length ns)
 
+-- | Get the currently-focused widget name from the ring. If the ring is
+-- emtpy, return 'Nothing'.
 focusGetCurrent :: FocusRing -> Maybe Name
 focusGetCurrent FocusRingEmpty = Nothing
 focusGetCurrent (FocusRingNonempty ns i) = Just $ ns !! i
 
-focusRingCursor :: (a -> FocusRing) -> a -> [CursorLocation] -> Maybe CursorLocation
+-- | Cursor selection convenience function for use as an
+-- 'Brick.Main.appChooseCursor' value.
+focusRingCursor :: (a -> FocusRing)
+                -- ^ The function used to get the focus ring out of your
+                -- application state.
+                -> a
+                -- ^ Your application state.
+                -> [CursorLocation]
+                -- ^ The list of available cursor positions.
+                -> Maybe CursorLocation
+                -- ^ The cursor position, if any, that matches the name
+                -- currently focused by the 'FocusRing'.
 focusRingCursor getRing st ls =
     listToMaybe $ filter isCurrent ls
     where
