19f5650a1bac96dd9f7eec81a1eb7ba4637e8c8d
Jonathan Daugherty <cygnus@foobox.com>
2015-07-07 19:42:56 -0700

    Merge the "Brick.Merge" module into Brick.List

--- src/Brick/Widgets/List.hs
+++ src/Brick/Widgets/List.hs
@@ -27,10 +27,10 @@ import Control.Applicative ((<$>))
 import Control.Lens ((^.), (&), (.~))
 import Data.Monoid ((<>))
 import Data.Maybe (fromMaybe)
+import Data.Algorithm.Diff
 import Graphics.Vty (Event(..), Key(..))
 
 import Brick.Types
-import Brick.Merge (maintainSel)
 import Brick.Widgets.Core
 import Brick.Util (clamp)
 import Brick.AttrMap
@@ -151,3 +151,28 @@ listSelectedElement :: List e -> Maybe (Int, e)
 listSelectedElement l = do
   sel <- l^.listSelectedL
   return (sel, (l^.listElementsL) !! sel)
+
+-- Assuming `xs` is an existing list that we want to update to match the state
+-- of `ys`. Given a selected index in `xs`, the goal is to compute the
+-- corresponding index in `ys`.
+maintainSel :: Eq e => [e] -> [e] -> Int -> Int
+maintainSel xs ys sel = let hunks = getDiff xs ys
+                        in merge 0 sel hunks
+
+merge :: Eq e => Int -> Int -> [Diff e] -> Int
+merge _   sel []                 = sel
+merge idx sel (h:hs) | idx > sel = sel
+                     | otherwise = case h of
+    Both _ _ -> merge sel (idx+1) hs
+
+    -- element removed in new list
+    First _  -> let newSel = if idx < sel
+                             then sel - 1
+                             else sel
+                in merge newSel idx hs
+
+    -- element added in new list
+    Second _ -> let newSel = if idx <= sel
+                             then sel + 1
+                             else sel
+                in merge newSel (idx+1) hs
