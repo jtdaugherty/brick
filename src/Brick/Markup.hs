7f85a1de448ea8a1b95a086f9b0f43db092423eb
Jonathan Daugherty <cygnus@foobox.com>
2015-07-07 19:30:57 -0700

    Types: add docstrings

--- src/Brick/Markup.hs
+++ src/Brick/Markup.hs
@@ -1,3 +1,8 @@
+-- | This module provides an API for turning "markup" values into
+-- widgets. This module uses the Data.Text.Markup interface in this
+-- package to assign attributes to substrings in a text string; to
+-- manipulate markup using (for example) syntax highlighters, see that
+-- module.
 module Brick.Markup
   ( Markup
   , markup
@@ -17,7 +22,10 @@ import Graphics.Vty (Attr, horizCat, string)
 import Brick.Widgets.Core
 import Brick.AttrMap
 
+-- | A type class for types that provide access to an attribute in the
+-- rendering monad.  You probably won't need to instance this.
 class GetAttr a where
+    -- | Where to get the attribute for this attribute metadata.
     getAttr :: a -> RenderM Attr
 
 instance GetAttr Attr where
@@ -28,9 +36,14 @@ instance GetAttr Attr where
 instance GetAttr AttrName where
     getAttr = lookupAttrName
 
+-- | Build a piece of markup from text with an assigned attribute name.
+-- When the markup is rendered, the attribute name will be looked up in
+-- the rendering context's 'AttrMap' to determine the attribute to use
+-- for this piece of text.
 (@?) :: T.Text -> AttrName -> Markup AttrName
 (@?) = (@@)
 
+-- | Build a widget from markup.
 markup :: (Eq a, GetAttr a) => Markup a -> Widget
 markup m =
     Widget Fixed Fixed $ do
--- src/Brick/Types.hs
+++ src/Brick/Types.hs
@@ -1,3 +1,4 @@
+-- | Basic types used by this library.
 {-# LANGUAGE TemplateHaskell #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 module Brick.Types
@@ -19,7 +20,9 @@ import Graphics.Vty (Event)
 import qualified Language.Haskell.TH.Syntax as TH
 import qualified Language.Haskell.TH.Lib as TH
 
+-- | A terminal screen location.
 data Location = Location { _loc :: (Int, Int)
+                         -- ^ (Column, Row)
                          }
                 deriving Show
 
@@ -31,20 +34,26 @@ instance Field1 Location Location Int Int where
 instance Field2 Location Location Int Int where
     _2 = loc._2
 
+-- | The class of types that behave like terminal locations.
 class TerminalLocation a where
+    -- | Get the column out of the value
     column :: Lens' a Int
+    -- | Get the row out of the value
     row :: Lens' a Int
 
 instance TerminalLocation Location where
     column = _1
     row = _2
 
+-- | Names of things. Used to name cursor locations, widgets, and
+-- viewports.
 newtype Name = Name String
              deriving (Eq, Show, Ord)
 
 instance IsString Name where
     fromString = Name
 
+-- | The origin (upper-left corner).
 origin :: Location
 origin = Location (0, 0)
 
@@ -52,9 +61,12 @@ instance Monoid Location where
     mempty = origin
     mappend (Location (w1, h1)) (Location (w2, h2)) = Location (w1+w2, h1+h2)
 
+-- | A cursor location.  These are returned by the rendering process.
 data CursorLocation =
     CursorLocation { _cursorLocation :: !Location
+                   -- ^ The location
                    , _cursorLocationName :: !(Maybe Name)
+                   -- ^ The name of the widget associated with the location
                    }
                    deriving Show
 
@@ -67,6 +79,11 @@ instance TerminalLocation CursorLocation where
 class HandleEvent a where
     handleEvent :: Event -> a -> a
 
+-- | A template haskell function to build lenses for a record type. This
+-- function differs from the 'Lens.makeLenses' function in that it does
+-- not require the record fields to be prefixed with underscores and
+-- it adds an "L" suffix to lens names to make it clear that they are
+-- lenses.
 suffixLenses :: TH.Name -> TH.DecsQ
 suffixLenses = makeLensesWith $
   lensRules & lensField .~ (\_ _ name -> [TopName $ TH.mkName $ TH.nameBase name ++ "L"])
