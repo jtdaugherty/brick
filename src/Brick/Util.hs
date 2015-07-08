86165da817d12b516f9ba5dff6a5eb707f78e313
Jonathan Daugherty <cygnus@foobox.com>
2015-07-07 17:05:37 -0700

    Brick.Util: Remove "for" and add docstrings

--- src/Brick/Util.hs
+++ src/Brick/Util.hs
@@ -14,21 +14,46 @@ import Graphics.Vty
 
 import Brick.Types (Location(..), CursorLocation(..), cursorLocation)
 
-for :: [a] -> (a -> b) -> [b]
-for = flip map
-
-clamp :: (Ord a) => a -> a -> a -> a
+-- | Given a minimum value and a maximum value, clamp a value to that
+-- range (values less than the minimum map to the minimum and values
+-- greater than the maximum map to the maximum).
+--
+-- >>> clamp 1 10 11
+-- 10
+-- >>> clamp 1 10 2
+-- 2
+-- >>> clamp 5 10 1
+-- 5
+clamp :: (Ord a)
+      => a
+      -- ^ The minimum value
+      -> a
+      -- ^ The maximum value
+      -> a
+      -- ^ The value to clamp
+      -> a
 clamp mn mx val = max mn (min val mx)
 
-on :: Color -> Color -> Attr
+-- | Build an attribute from a foreground color and a background color.
+-- Intended to be used infix.
+on :: Color
+   -- ^ The foreground color
+   -> Color
+   -- ^ The background color
+   -> Attr
 on f b = defAttr `withForeColor` f
                  `withBackColor` b
 
+-- | Create an attribute from the specified foreground color (the
+-- background color is the "default").
 fg :: Color -> Attr
 fg = (defAttr `withForeColor`)
 
+-- | Create an attribute from the specified background color (the
+-- background color is the "default").
 bg :: Color -> Attr
 bg = (defAttr `withBackColor`)
 
+-- | Add a 'Location' offset to the specified 'CursorLocation'.
 clOffset :: CursorLocation -> Location -> CursorLocation
 clOffset cl loc = cl & cursorLocation %~ ( <> loc)
