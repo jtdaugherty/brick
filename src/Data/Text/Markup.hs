a6d6561d3c3c77d1b5324f602e519341a9b95d0d
Jonathan Daugherty <cygnus@foobox.com>
2015-07-07 20:14:37 -0700

    Data.Text.Markup: docstrings

--- src/Data/Text/Markup.hs
+++ src/Data/Text/Markup.hs
@@ -1,3 +1,10 @@
+-- | This module provides an API for "marking up" text with arbitrary
+-- values. A piece of markup can then be converted to a list of pairs
+-- representing the sequences of characters assigned the same markup
+-- value.
+--
+-- This interface is experimental. Don't use this for your full-file
+-- syntax highlighter just yet!
 module Data.Text.Markup
   ( Markup
   , markupToList
@@ -15,6 +22,7 @@ import Data.Monoid
 import Data.String (IsString(..))
 import qualified Data.Text as T
 
+-- | Markup with metadata type 'a' assigned to each character.
 data Markup a = Markup [(Char, a)]
               deriving Show
 
@@ -26,15 +34,21 @@ instance Monoid (Markup a) where
 instance (Default a) => IsString (Markup a) where
     fromString = fromText . T.pack
 
+-- | Build a piece of markup; assign the specified metadata to every
+-- character in the specified text.
 (@@) :: T.Text -> a -> Markup a
 t @@ val = Markup [(c, val) | c <- T.unpack t]
 
+-- | Build markup from text with the default metadata.
 fromText :: (Default a) => T.Text -> Markup a
 fromText = (@@ def)
 
+-- | Extract the text from markup, discarding the markup metadata.
 toText :: (Eq a) => Markup a -> T.Text
 toText = T.concat . (fst <$>) . markupToList
 
+-- | Set the metadata for a range of character positions in a piece of
+-- markup. This is useful for, e.g., syntax highlighting.
 markupSet :: (Eq a) => (Int, Int) -> a -> Markup a -> Markup a
 markupSet (start, len) val m@(Markup l) = if start < 0 || start + len > length l
                                           then m
@@ -45,6 +59,8 @@ markupSet (start, len) val m@(Markup l) = if start < 0 || start + len > length l
         (theOldEntries, theTail) = splitAt len theLongTail
         theNewEntries = zip (fst <$> theOldEntries) (repeat val)
 
+-- | Convert markup to a list of pairs in which each pair contains the
+-- longest subsequence of characters having the same metadata.
 markupToList :: (Eq a) => Markup a -> [(T.Text, a)]
 markupToList (Markup thePairs) = toList thePairs
     where
@@ -53,5 +69,6 @@ markupToList (Markup thePairs) = toList thePairs
             where
                 (matching, remaining) = break (\(_, v) -> v /= val) rest
 
+-- | Convert a list of text and metadata pairs into markup.
 fromList :: [(T.Text, a)] -> Markup a
 fromList pairs = Markup $ concatMap (\(t, val) -> [(c, val) | c <- T.unpack t]) pairs
