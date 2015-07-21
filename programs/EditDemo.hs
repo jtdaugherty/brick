935f05973a3ca26663fe6f212d9227532c30461b
Jonathan Daugherty <cygnus@foobox.com>
2015-07-20 18:31:33 -0700

    Update editor demo to show both limited and unlimited editors

--- programs/EditDemo.hs
+++ programs/EditDemo.hs
@@ -1,6 +1,10 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE RankNTypes #-}
 module Main where
 
+import Control.Lens
+import Data.Maybe (listToMaybe)
 import qualified Graphics.Vty as V
 
 import qualified Brick.Main as M
@@ -8,6 +12,7 @@ import qualified Brick.Types as T
 import Brick.Widgets.Core
   ( Widget
   , (<+>)
+  , (<=>)
   , hLimit
   , vLimit
   , str
@@ -17,29 +22,68 @@ import qualified Brick.Widgets.Edit as E
 import qualified Brick.AttrMap as A
 import Brick.Util (on)
 
-drawUI :: E.Editor -> [Widget]
-drawUI e = [ui]
+data St =
+    St { _currentEditor :: T.Name
+       , _edit1 :: E.Editor
+       , _edit2 :: E.Editor
+       }
+
+makeLenses ''St
+
+firstEditor :: T.Name
+firstEditor = "edit1"
+
+secondEditor :: T.Name
+secondEditor = "edit2"
+
+switchEditors :: St -> St
+switchEditors st =
+    if st^.currentEditor == firstEditor
+    then st & currentEditor .~ secondEditor
+    else st & currentEditor .~ firstEditor
+
+currentEditorL :: St -> Lens' St E.Editor
+currentEditorL st =
+    if st^.currentEditor == firstEditor
+    then edit1
+    else edit2
+
+drawUI :: St -> [Widget]
+drawUI st = [ui]
     where
-        ui = C.center $ "Input: " <+> (hLimit 30 $ vLimit 5 $ E.renderEditor e)
+        ui = C.center $ ("Input 1 (unlimited): " <+> (hLimit 30 $ vLimit 5 $ E.renderEditor $ st^.edit1)) <=>
+                        " " <=>
+                        ("Input 2 (limited to 2 lines): " <+> (hLimit 30 $ E.renderEditor $ st^.edit2)) <=>
+                        " " <=>
+                        "Press Tab to switch between editors, Esc to quit."
 
-appEvent :: E.Editor -> V.Event -> M.EventM (M.Next E.Editor)
-appEvent e ev =
+appEvent :: St -> V.Event -> M.EventM (M.Next St)
+appEvent st ev =
     case ev of
-        V.EvKey V.KEsc [] -> M.halt e
-        _ -> M.continue $ T.handleEvent ev e
+        V.EvKey V.KEsc [] -> M.halt st
+        V.EvKey (V.KChar '\t') [] -> M.continue $ switchEditors st
+        _ -> M.continue $ st & currentEditorL st %~ T.handleEvent ev
 
-initialState :: E.Editor
-initialState = E.editor (T.Name "edit") (str . unlines) Nothing ""
+initialState :: St
+initialState =
+    St firstEditor
+       (E.editor firstEditor (str . unlines) Nothing "")
+       (E.editor secondEditor (str . unlines) (Just 2) "")
 
 theMap :: A.AttrMap
 theMap = A.attrMap V.defAttr
     [ (E.editAttr, V.white `on` V.blue)
     ]
 
-theApp :: M.App E.Editor V.Event
+appCursor :: St -> [T.CursorLocation] -> Maybe T.CursorLocation
+appCursor st locs =
+    let matchesCurrentEditor loc = loc^.(T.cursorLocationNameL) == Just (st^.currentEditor)
+    in listToMaybe $ filter matchesCurrentEditor locs
+
+theApp :: M.App St V.Event
 theApp =
     M.App { M.appDraw = drawUI
-          , M.appChooseCursor = M.showFirstCursor
+          , M.appChooseCursor = appCursor
           , M.appHandleEvent = appEvent
           , M.appStartEvent = return
           , M.appAttrMap = const theMap
@@ -48,6 +92,8 @@ theApp =
 
 main :: IO ()
 main = do
-    e <- M.defaultMain theApp initialState
-    putStrLn "You entered:\n"
-    putStrLn $ unlines $ E.getEditContents e
+    st <- M.defaultMain theApp initialState
+    putStrLn "In input 1 you entered:\n"
+    putStrLn $ unlines $ E.getEditContents $ st^.edit1
+    putStrLn "In input 2 you entered:\n"
+    putStrLn $ unlines $ E.getEditContents $ st^.edit2
