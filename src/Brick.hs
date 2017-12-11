a59e1e279803b328d85a029c816b35ca3c1e5a91
Jonathan Daugherty <cygnus@foobox.com>
2017-12-10 21:23:39 -0800

    Brick: haddock update

--- src/Brick.hs
+++ src/Brick.hs
@@ -1,5 +1,8 @@
 -- | This module is provided as a convenience to import the most
--- important parts of the API all at once.
+-- important parts of the API all at once. Note that the Haddock
+-- documentation for this library is for /reference/ usage; if you
+-- are looking for an introduction or tutorial, see the README for links
+-- to plenty of material!
 module Brick
   ( module Brick.Main
   , module Brick.Types
