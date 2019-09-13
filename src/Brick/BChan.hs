e0fdfc77864ac3cbbd9bcf573e25aa2ee24b7d9e
Jonathan Daugherty <cygnus@foobox.com>
2019-09-13 10:20:12 -0700

    BChan: add writeBChanNonBlocking, raise STM lower bound to 2.4.3

--- src/Brick/BChan.hs
+++ src/Brick/BChan.hs
@@ -2,6 +2,7 @@ module Brick.BChan
   ( BChan
   , newBChan
   , writeBChan
+  , writeBChanNonBlocking
   , readBChan
   , readBChan2
   )
@@ -25,6 +26,16 @@ newBChan size = atomically $ BChan <$> newTBQueue (fromIntegral size)
 -- | Writes a value to a @BChan@; blocks if the channel is full.
 writeBChan :: BChan a -> a -> IO ()
 writeBChan (BChan q) a = atomically $ writeTBQueue q a
+
+-- | Attempts to write a value to a @BChan@. If the channel has room,
+-- the value is written and this returns 'True'. Otherwise this returns
+-- 'False' and returns immediately.
+writeBChanNonBlocking :: BChan a -> a -> IO Bool
+writeBChanNonBlocking (BChan q) a = atomically $ do
+    f <- isFullTBQueue q
+    if f
+       then return False
+       else writeTBQueue q a >> return True
 
 -- | Reads the next value from the @BChan@; blocks if necessary.
 readBChan :: BChan a -> IO a
