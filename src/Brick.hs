f141562756a16a337b604a14dd04bf2682923b0b
Jonathan Daugherty <cygnus@foobox.com>
2021-11-20 15:45:33 -0800

    Brick: improve docs (w.r.t. #349)

--- src/Brick.hs
+++ src/Brick.hs
@@ -1,8 +1,11 @@
 -- | This module is provided as a convenience to import the most
--- important parts of the API all at once. Note that the Haddock
--- documentation for this library is for /reference/ usage; if you
--- are looking for an introduction or tutorial, see the README for links
--- to plenty of material!
+-- important parts of the API all at once. If you are new to Brick and
+-- are looking to learn it, the best place to start is the
+-- [Brick User Guide](https://github.com/jtdaugherty/brick/blob/master/docs/guide.rst).
+-- The README also has links to other learning resources. Unlike
+-- most Haskell libraries that only have API documentation, Brick
+-- is best larned by reading the User Guide and other materials and
+-- referring to the API docs only as needed. Enjoy!
 module Brick
   ( module Brick.Main
   , module Brick.Types
