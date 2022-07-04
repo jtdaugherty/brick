5993603b0250868471a7a7c44fbef7fee7f1f893
Jonathan Daugherty <cygnus@foobox.com>
2022-07-04 08:53:35 -0700

    Render.hs test program: various enhancements

--- tests/Render.hs
+++ tests/Render.hs
@@ -17,23 +17,28 @@ renderDisplay ws = do
     outp <- V.outputForConfig V.defaultConfig
     ctx <- V.displayContext outp region
     V.outputPicture ctx (renderWidget Nothing ws region)
+    V.releaseDisplay outp
 
 myWidget :: Widget ()
 myWidget = str "Why" <=> hBorder <=> str "not?"
 
 -- Since you can't Read a Picture, we have to compare the result with
 -- the Shown one
-renderedMyWidget :: String
-renderedMyWidget = "Picture ?? [VertJoin {partTop = VertJoin {partTop = HorizText {attr = Attr {attrStyle = Default, attrForeColor = Default, attrBackColor = Default, attrURL = Default}, displayText = \"Why                           \", outputWidth = 30, charWidth = 30}, partBottom = VertJoin {partTop = HorizText {attr = Attr {attrStyle = Default, attrForeColor = Default, attrBackColor = Default, attrURL = Default}, displayText = \"\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\", outputWidth = 30, charWidth = 30}, partBottom = HorizText {attr = Attr {attrStyle = Default, attrForeColor = Default, attrBackColor = Default, attrURL = Default}, displayText = \"not?                          \", outputWidth = 30, charWidth = 30}, outputWidth = 30, outputHeight = 2}, outputWidth = 30, outputHeight = 3}, partBottom = BGFill {outputWidth = 30, outputHeight = 7}, outputWidth = 30, outputHeight = 10}] ??"
+expectedResult :: String
+expectedResult = "Picture ?? [VertJoin {partTop = VertJoin {partTop = HorizText {attr = Attr {attrStyle = Default, attrForeColor = Default, attrBackColor = Default, attrURL = Default}, displayText = \"Why                           \", outputWidth = 30, charWidth = 30}, partBottom = VertJoin {partTop = HorizText {attr = Attr {attrStyle = Default, attrForeColor = Default, attrBackColor = Default, attrURL = Default}, displayText = \"\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\\9472\", outputWidth = 30, charWidth = 30}, partBottom = HorizText {attr = Attr {attrStyle = Default, attrForeColor = Default, attrBackColor = Default, attrURL = Default}, displayText = \"not?                          \", outputWidth = 30, charWidth = 30}, outputWidth = 30, outputHeight = 2}, outputWidth = 30, outputHeight = 3}, partBottom = BGFill {outputWidth = 30, outputHeight = 7}, outputWidth = 30, outputHeight = 10}] ??"
 
 main :: IO Bool
 main = do
     result <- try (renderDisplay [myWidget]) :: IO (Either V.VtyConfigurationError ())
     case result of
-      Left _ -> putStrLn "Terminal is not available"
-      Right () -> return ()
+        Left _ -> do
+            putStrLn "Terminal is not available"
+            return False
+        Right () -> do
+            let matched = actualResult == expectedResult
+                actualResult = show (renderWidget Nothing [myWidget] region)
+                msg = if matched then "rendering match" else "rendering mismatch"
 
-    print $ show $ renderWidget Nothing [myWidget] region
-
-    return $
-      show (renderWidget Nothing [myWidget] region) == renderedMyWidget
+            putStrLn ""
+            putStrLn $ "renderWidget test outcome: " <> msg
+            return matched
