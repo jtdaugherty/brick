2a99ee3fa04a2bf6169f74e69869b1a32d4b496e
Jonathan Daugherty <cygnus@foobox.com>
2022-07-09 08:05:49 -0700

    Whitespace

--- src/Brick/Widgets/Edit.hs
+++ src/Brick/Widgets/Edit.hs
@@ -115,47 +115,49 @@ instance DecodeUtf8 String where
     decodeUtf8 bs = T.unpack <$> decodeUtf8 bs
 
 handleEditorEvent :: (DecodeUtf8 t, Eq t, Z.GenericTextZipper t)
-                  => Event -> Editor t n -> EventM n (Editor t n)
+                  => Event
+                  -> Editor t n
+                  -> EventM n (Editor t n)
 handleEditorEvent e ed =
-        let f = case e of
-                  EvPaste bs -> case decodeUtf8 bs of
-                      Left _ -> id
-                      Right t -> Z.insertMany t
-                  EvKey (KChar 'a') [MCtrl] -> Z.gotoBOL
-                  EvKey (KChar 'e') [MCtrl] -> Z.gotoEOL
-                  EvKey (KChar 'd') [MCtrl] -> Z.deleteChar
-                  EvKey (KChar 'd') [MMeta] -> Z.deleteWord
-                  EvKey (KChar 'k') [MCtrl] -> Z.killToEOL
-                  EvKey (KChar 'u') [MCtrl] -> Z.killToBOL
-                  EvKey KEnter [] -> Z.breakLine
-                  EvKey KDel [] -> Z.deleteChar
-                  EvKey (KChar c) [] | c /= '\t' -> Z.insertChar c
-                  EvKey KUp [] -> Z.moveUp
-                  EvKey KDown [] -> Z.moveDown
-                  EvKey KLeft [] -> Z.moveLeft
-                  EvKey KRight [] -> Z.moveRight
-                  EvKey (KChar 'b') [MCtrl] -> Z.moveLeft
-                  EvKey (KChar 'f') [MCtrl] -> Z.moveRight
-                  EvKey (KChar 'b') [MMeta] -> Z.moveWordLeft
-                  EvKey (KChar 'f') [MMeta] -> Z.moveWordRight
-                  EvKey KBS [] -> Z.deletePrevChar
-                  EvKey (KChar 't') [MCtrl] -> Z.transposeChars
-                  EvKey KHome [] -> Z.gotoBOL
-                  EvKey KEnd [] -> Z.gotoEOL
-                  EvKey (KChar '<') [MMeta] -> Z.gotoBOF
-                  EvKey (KChar '>') [MMeta] -> Z.gotoEOF
-                  _ -> id
-        in return $ applyEdit f ed
+    let f = case e of
+              EvPaste bs -> case decodeUtf8 bs of
+                  Left _ -> id
+                  Right t -> Z.insertMany t
+              EvKey (KChar 'a') [MCtrl] -> Z.gotoBOL
+              EvKey (KChar 'e') [MCtrl] -> Z.gotoEOL
+              EvKey (KChar 'd') [MCtrl] -> Z.deleteChar
+              EvKey (KChar 'd') [MMeta] -> Z.deleteWord
+              EvKey (KChar 'k') [MCtrl] -> Z.killToEOL
+              EvKey (KChar 'u') [MCtrl] -> Z.killToBOL
+              EvKey KEnter [] -> Z.breakLine
+              EvKey KDel [] -> Z.deleteChar
+              EvKey (KChar c) [] | c /= '\t' -> Z.insertChar c
+              EvKey KUp [] -> Z.moveUp
+              EvKey KDown [] -> Z.moveDown
+              EvKey KLeft [] -> Z.moveLeft
+              EvKey KRight [] -> Z.moveRight
+              EvKey (KChar 'b') [MCtrl] -> Z.moveLeft
+              EvKey (KChar 'f') [MCtrl] -> Z.moveRight
+              EvKey (KChar 'b') [MMeta] -> Z.moveWordLeft
+              EvKey (KChar 'f') [MMeta] -> Z.moveWordRight
+              EvKey KBS [] -> Z.deletePrevChar
+              EvKey (KChar 't') [MCtrl] -> Z.transposeChars
+              EvKey KHome [] -> Z.gotoBOL
+              EvKey KEnd [] -> Z.gotoEOL
+              EvKey (KChar '<') [MMeta] -> Z.gotoBOF
+              EvKey (KChar '>') [MMeta] -> Z.gotoEOF
+              _ -> id
+    in return $ applyEdit f ed
 
 -- | Construct an editor over 'Text' values
 editorText :: n
-       -- ^ The editor's name (must be unique)
-       -> Maybe Int
-       -- ^ The limit on the number of lines in the editor ('Nothing'
-       -- means no limit)
-       -> T.Text
-       -- ^ The initial content
-       -> Editor T.Text n
+           -- ^ The editor's name (must be unique)
+           -> Maybe Int
+           -- ^ The limit on the number of lines in the editor ('Nothing'
+           -- means no limit)
+           -> T.Text
+           -- ^ The initial content
+           -> Editor T.Text n
 editorText = editor
 
 -- | Construct an editor over 'String' values
