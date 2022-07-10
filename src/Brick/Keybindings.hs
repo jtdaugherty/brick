232fcc81b4b7953b1f54c4b7cc75f65d8aff2ee2
Jonathan Daugherty <cygnus@foobox.com>
2022-07-10 09:50:09 -0700

    Keybindings: re-export Pretty module

--- src/Brick/Keybindings.hs
+++ src/Brick/Keybindings.hs
@@ -2,9 +2,11 @@ module Brick.Keybindings
   ( module Brick.Keybindings.KeyEvents
   , module Brick.Keybindings.KeyConfig
   , module Brick.Keybindings.KeyHandlerMap
+  , module Brick.Keybindings.Pretty
   )
 where
 
 import Brick.Keybindings.KeyEvents
 import Brick.Keybindings.KeyConfig
 import Brick.Keybindings.KeyHandlerMap
+import Brick.Keybindings.Pretty
