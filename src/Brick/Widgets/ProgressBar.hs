71de7af07071e9a5ea417acff16a282262c84363
jk <47693+sectore@users.noreply.github.com>
2024-10-25 14:34:23 +0200

    `ProgressBar` variants

--- src/Brick/Widgets/ProgressBar.hs
+++ src/Brick/Widgets/ProgressBar.hs
@@ -1,24 +1,26 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE CPP #-}
+{-# LANGUAGE FlexibleInstances #-}
+
 -- | This module provides a progress bar widget.
 module Brick.Widgets.ProgressBar
   ( progressBar
+  , progressBar'
   -- * Attributes
   , progressCompleteAttr
   , progressIncompleteAttr
   )
 where
 
-import Lens.Micro ((^.))
 import Data.Maybe (fromMaybe)
+import Lens.Micro ((^.))
 #if !(MIN_VERSION_base(4,11,0))
 import Data.Monoid
 #endif
-import Graphics.Vty (safeWcswidth)
-
-import Brick.Types
 import Brick.AttrMap
+import Brick.Types
 import Brick.Widgets.Core
+import Graphics.Vty (safeWcswidth)
 
 -- | The attribute of the completed portion of the progress bar.
 progressCompleteAttr :: AttrName
@@ -38,16 +40,41 @@ progressBar :: Maybe String
             -- ^ The progress value. Should be between 0 and 1 inclusive.
             -> Widget n
 progressBar mLabel progress =
+    progressBar' mLabel progress (' ', ' ')
+
+-- | Draw a progress bar with the specified (optional) label,
+-- progress value and a pair of characters to fill the progress. 
+-- This fills available horizontal space and is one row high.
+progressBar' :: Maybe String
+             -- ^ The label. If specified, this is shown in the center of
+             -- the progress bar.
+             -> Float
+             -- ^ The progress value. Should be between 0 and 1 inclusive.
+             -> (Char, Char)
+             -- ^ Pair of characters to fill the bar. First character is used 
+             -- to fill the completed part, second character draws the uncompleted part.
+             -- Please be aware of using wide characters in Brick, 
+             -- see [Wide Character Support and the TextWidth class](https://github.com/jtdaugherty/brick/blob/master/docs/guide.rst#wide-character-support-and-the-textwidth-class)
+             -> Widget n
+progressBar' mLabel progress (completeChar, uncompleteChar) =
     Widget Greedy Fixed $ do
         c <- getContext
-        let barWidth = c^.availWidthL
+        let barWidth = c ^. availWidthL
             label = fromMaybe "" mLabel
             labelWidth = safeWcswidth label
             spacesWidth = barWidth - labelWidth
-            leftPart = replicate (spacesWidth `div` 2) ' '
-            rightPart = replicate (barWidth - (labelWidth + length leftPart)) ' '
+            leftWidth = spacesWidth `div` 2
+            rightWidth = barWidth - labelWidth - leftWidth
+            completeWidth = round $ progress * toEnum barWidth
+            
+            leftCompleteWidth = min leftWidth completeWidth
+            leftIncompleteWidth = leftWidth - leftCompleteWidth         
+            leftPart = replicate leftCompleteWidth completeChar ++ replicate leftIncompleteWidth uncompleteChar
+            rightCompleteWidth = max 0 (completeWidth - labelWidth - leftWidth)
+            rightIncompleteWidth = rightWidth - rightCompleteWidth
+            rightPart = replicate rightCompleteWidth completeChar ++ replicate rightIncompleteWidth uncompleteChar
+    
             fullBar = leftPart <> label <> rightPart
-            completeWidth = round $ progress * toEnum (length fullBar)
             adjustedCompleteWidth = if completeWidth == length fullBar && progress < 1.0
                                     then completeWidth - 1
                                     else if completeWidth == 0 && progress > 0.0
