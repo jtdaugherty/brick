jtdaugherty/brick

commit 696ae5c467cf5e8da7d3f5f5a5356f26210e1117

renderFinal: reset render-specific state in between renderings to avoid preservation of stale state (extents, etc) across renderings

src/Brick/Widgets/Internal.hs
@@ -8,7 +8,7 @@ module Brick.Widgets.Internal
   )
 where
 
-import Lens.Micro ((^.), (&), (%~))
+import Lens.Micro ((^.), (&), (%~), (.~))
 import Lens.Micro.Mtl ((%=))
 import Control.Monad
 import Control.Monad.State.Strict
@@ -35,7 +35,14 @@ renderFinal :: (Ord n)
 renderFinal aMap layerRenders (w, h) chooseCursor rs =
     (newRS, picWithBg, theCursor, concat layerExtents)
     where
-        (layerResults, !newRS) = flip runState rs $ sequence $
+        -- Reset various fields from the last rendering state so they
+        -- don't accumulate or affect this rendering.
+        resetRs = rs & reportedExtentsL .~ mempty
+                     & observedNamesL .~ mempty
+                     & clickableNamesL .~ mempty
+                     & requestedVisibleNames_L .~ mempty
+
+        (layerResults, !newRS) = flip runState resetRs $ sequence $
             (\p -> runReaderT p ctx) <$>
             (\layerWidget -> do
                 result <- render $ cropToContext layerWidget

commit 5c89b531fee8330b03a6f4db32de6ce7ab4f2e23

Brick.Widgets.Internal.renderFinal: do not clear visibility requests from RenderState
This fixes a bug introduced in 696ae5c467cf5e8da7d3f5f5a5356f26210e1117
that cleared the visibility requests from the RenderState. While
other state fields should indeed be cleared, this particular field is
populated from EventM state prior to calling renderFinal. Without this,
calls to makeVisible had no effect, completely breaking that approach to
managing viewport scrolling.

src/Brick/Widgets/Internal.hs
@@ -40,7 +40,6 @@ renderFinal aMap layerRenders (w, h) chooseCursor rs =
         resetRs = rs & reportedExtentsL .~ mempty
                      & observedNamesL .~ mempty
                      & clickableNamesL .~ mempty
-                     & requestedVisibleNames_L .~ mempty
 
         (layerResults, !newRS) = flip runState resetRs $ sequence $
             (\p -> runReaderT p ctx) <$>