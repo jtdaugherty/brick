a32d140dac99856fc0b9e8594ded155ad1530170
Jonathan Daugherty <cygnus@foobox.com>
2026-09-12 11:39:54 -0700

    MenuBarDemo: add help text about left/right arrow keys

--- programs/MenuBarDemo.hs
+++ programs/MenuBarDemo.hs
@@ -43,8 +43,10 @@ drawUi st =
       Text.unlines $
       [ "Click the menu title with the mouse or press Alt-F to open the menu."
       , ""
-      , "When the menu is open, press arrow keys to select items and then " <>
+      , "When the menu is open, press up/down arrow keys to select items and then " <>
         "press Enter to activate them, or click them with the mouse instead."
+      , ""
+      , "When a menu is open, press left/right arrow keys cycle through open menus."
       , ""
       , "Press Esc to quit the program."
       ]
