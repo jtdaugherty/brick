f931591e6be465fa09a7f975e05711ec8853bc01
Jonathan Daugherty <cygnus@foobox.com>
2026-09-23 10:49:23 -0700

    Menu: haddock edit

--- src/Brick/Widgets/Menu.hs
+++ src/Brick/Widgets/Menu.hs
@@ -3,10 +3,10 @@
 {-# LANGUAGE RankNTypes #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# OPTIONS_GHC -fno-warn-unused-top-binds #-}
--- | This module provides a drop-down menu widget that is common to
--- most graphical interface toolkits. Menus carry entries that can be
--- activated with the mouse and keyboard and invoke event handlers that
--- you specify when creating the menus and entries.
+-- | This module provides a menu widget that is similar to the ones
+-- commonly found in most graphical interface toolkits. Menus carry
+-- entries that can be activated with the mouse and keyboard and invoke
+-- event handlers that you specify when creating the menus and entries.
 --
 -- This module provides a fully general 'Menu' type and a few
 -- specialized interfaces for common menu use cases:
